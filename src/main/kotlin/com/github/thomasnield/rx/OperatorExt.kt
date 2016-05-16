182862bd7fbf21863a302d605e2738f35cf04424
Thomas Nield <thomasnield@live.com>
2016-05-16 12:20:32 -0500

    experiment with DSL for multiple lambda arguments

--- src/main/kotlin/com/github/thomasnield/rx/OperatorExt.kt
+++ src/main/kotlin/com/github/thomasnield/rx/OperatorExt.kt
@@ -13,3 +13,16 @@ fun <T> Observable<T>.toListUntileChanged() =  compose(Transformers.toListUntilC
 inline fun <T,R: Collection<T>> Observable<T>.collectWhile(crossinline factory: () -> R, crossinline condition: (R, T) -> Boolean) =
         compose(Transformers.collectWhile({factory.invoke()}, { r,t -> condition.invoke(r,t)}))
 
+
+fun <T,R: Collection<T>> Observable<T>.collectWhile(op: CollectWhileParams<T,R>.() -> Unit): Observable<R> {
+    val params = CollectWhileParams<T,R>()
+    params.op()
+    return this.collectWhile(params.factory!!,params.condition!!)
+}
+class CollectWhileParams<T,R> {
+    internal var factory: (() -> R)? = null
+    internal var condition: ((R,T) -> Boolean)? = null
+
+    fun factory(factory: () -> R) { this.factory = factory }
+    fun condition(condition: (R,T) -> Boolean) { this.condition = condition }
+}
