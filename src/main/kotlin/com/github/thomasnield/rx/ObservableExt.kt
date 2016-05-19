61c3c9a80f32f46a0ea134412bb9ad770d0e94dd
Thomas Nield <thomasnield@live.com>
2016-05-19 12:34:21 -0500

    Add more operator extensions, including cache()

--- src/main/kotlin/com/github/thomasnield/rx/ObservableExt.kt
+++ src/main/kotlin/com/github/thomasnield/rx/ObservableExt.kt
@@ -1,7 +1,11 @@
 package com.github.thomasnield.rx
 
+import com.github.davidmoten.rx.Obs
 import com.github.davidmoten.rx.Transformers
 import rx.Observable
+import rx.Scheduler
+import rx.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 
 inline fun <T> Observable<T>.toListWhile(crossinline condition: (MutableList<in T>, T) -> Boolean) =
@@ -19,6 +23,18 @@ fun <T,R: Collection<T>> Observable<T>.collectWhile(op: CollectWhileParams<T,R>.
     params.op()
     return this.collectWhile(params.factory!!,params.condition!!)
 }
+
+fun <T> Observable<T>.cache(duration: Long, unit: TimeUnit, worker: Scheduler.Worker) =
+        Obs.cache(this,duration,unit,worker)
+
+fun <T> Observable<T>.cache(duration: Long, unit: TimeUnit, scheduler: Scheduler = Schedulers.computation()) =
+        Obs.cache(this,duration,unit,scheduler)
+
+inline fun <T> Observable<T>.doOnNext(n: Int, crossinline action: (T) -> Unit) = compose(Transformers.doOnNext(n,{action.invoke(it)}))
+inline fun <T> Observable<T>.doOnFirst(crossinline action: (T) -> Unit) = compose(Transformers.doOnFirst{ action.invoke(it) })
+fun <R,T> Observable<T>.ignoreElementsThen(next: Observable<R>) = compose(Transformers.ignoreElementsThen(next))
+
+
 class CollectWhileParams<T,R> {
     internal var factory: (() -> R)? = null
     internal var condition: ((R,T) -> Boolean)? = null
