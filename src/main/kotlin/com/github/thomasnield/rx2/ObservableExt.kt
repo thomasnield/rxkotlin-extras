805a5ed43f38861e22ee23006e0556627a58834d
Thomas Nield <thomasnield@live.com>
2017-03-14 11:35:43 -0500

    upgrade to RxJava2

--- src/main/kotlin/com/github/thomasnield/rx2/ObservableExt.kt
+++ src/main/kotlin/com/github/thomasnield/rx2/ObservableExt.kt
@@ -1,14 +1,8 @@
-package com.github.thomasnield.rx
+package com.github.thomasnield.rx2
 
-import com.github.davidmoten.rx.Bytes
-import com.github.davidmoten.rx.Obs
-import com.github.davidmoten.rx.Transformers
-import rx.Observable
-import rx.Scheduler
-import rx.schedulers.Schedulers
+import com.github.davidmoten.rx2.Bytes
 import java.io.File
 import java.io.InputStream
-import java.util.concurrent.TimeUnit
 
 
 fun InputStream.toBytesObservable(chunkSize: Int) = Bytes.from(this,chunkSize)
@@ -16,28 +10,19 @@ fun File.toBytesObservable(chunkSize: Int) = Bytes.from(this,chunkSize)
 fun InputStream.toBytesUnzipObservable() = Bytes.unzip(this)
 fun File.toBytesUnzipObservable() = Bytes.unzip(this)
 
+/*
 fun <T> Observable<T>.cache(duration: Long, unit: TimeUnit, worker: Scheduler.Worker) =
         Obs.cache(this,duration,unit,worker)
 
 fun <T> Observable<T>.cache(duration: Long, unit: TimeUnit, scheduler: Scheduler = Schedulers.computation()) =
         Obs.cache(this,duration,unit,scheduler)
+*/
 
+/*
 
 fun <T> Observable<T>.doOnEmpty(onEmpty: () -> Unit) = compose(Transformers.doOnEmpty(onEmpty))
 fun <T> Observable<T>.doOnFirst(action: (T) -> Unit) = compose(Transformers.doOnFirst(action))
 
 fun <T> Observable<T>.doOnNext(n: Int, action: (T) -> Unit) = compose(Transformers.doOnNext(n,action))
-
-//TODO
-//fun <T> Observable<T>.retryWhen() =
-
-fun <T> Observable<T>.toListWhile(condition: (MutableList<T>, T) -> Boolean): Observable<List<T>> =
-         compose(Transformers.toListWhile(condition))
-
-fun <T> Observable<T>.toListUntilChanged() = compose(Transformers.toListUntilChanged())
-
-fun <T,R: MutableCollection<T>> Observable<T>.collectWhile(factory: (() -> R), action: (R,T) -> Unit, condition: (R,T) -> Boolean) =
-        compose(Transformers.collectWhile(factory,action,condition))
-
-fun <R,T> Observable<T>.ignoreElementsThen(next: Observable<R>) = compose(Transformers.ignoreElementsThen(next))
+*/
 
